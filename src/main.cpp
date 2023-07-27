75f0ecd093feaf833e34d943f589ec615d9081e3
Like4Schnitzel <rw21062006@gmail.com>
2023-07-27 21:14:19 +0200

    create hands

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 bool simulateGame(const int players)
 {
+    int cardsInHand;
+    if (players < 3) cardsInHand = 7;
+    else cardsInHand = 6;
+
+    //populate and shuffle stack
     vector<int> stack;
     for (int i = 2; i <= 99; i++)
     {
@@ -15,6 +20,25 @@ bool simulateGame(const int players)
     }
     random_shuffle(stack.begin(), stack.end());
 
+    //give initial cards to players
+    vector<vector<int>> hands;
+    for (int i = 0; i < players; i++)
+    {
+        hands.push_back(vector<int>());
+        for (int j = 0; j < cardsInHand; j++)
+        {
+            hands[i].push_back(stack.back());
+            stack.pop_back();
+        }
+    }
+
+    //main game loop
+    int current_player = 0;
+    while (stack.size() > 0)
+    {
+    }
+
+    //win
     return true;
 }
 
@@ -23,9 +47,11 @@ int main()
     int players;
     do 
     {
-        cout << "Enter amount of players (greater than 0): ";
+        cout << "Enter amount of players (1-5): ";
         cin >> players;
-    } while (players <= 0);
+    } while (players < 1 || players > 5);
+
+    simulateGame(players);
 
     return 0;
 }
